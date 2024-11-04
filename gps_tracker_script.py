779571ad2669f83d5e3372af5dd17052abc568db
Deepesh kumar <89071341+Dk5058203@users.noreply.github.com>
2024-11-04 09:00:54 +0530

    Create gps_tracker_script.py
    added a .py file

---
+++ gps_tracker_script.py
@@ -0,0 +1,4 @@
+print("A online webpage with Front-end login based GPS tracker system.")
+print("- Designed using HTML, CSS, and SQLite")
+print("- Tracks the location of the bus route with latitude and longitude from Google Maps APIs.")
+print("Other contributors")
