d7f1332a10192aa5339c28d01f228a5aba42922d
Deepesh kumar <89071341+Dk5058203@users.noreply.github.com>
2024-11-04 21:07:01 +0530

    Update gps_tracker_script.py

--- gps_tracker_script.py
+++ gps_tracker_script.py
@@ -2,3 +2,13 @@ print("A online webpage with Front-end login based GPS tracker system.")
 print("- Designed using HTML, CSS, and SQLite")
 print("- Tracks the location of the bus route with latitude and longitude from Google Maps APIs.")
 print("Other contributors")
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "CICD PIPELINE SAMPLE FOR A PROJECT "
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8080)
